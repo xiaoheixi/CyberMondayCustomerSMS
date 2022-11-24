df1e7f978e3662cb089bdd4c8259a8aebfb82ef6
xiaoheixi <justinzhao893@gmail.com>
2022-11-25 09:47:24 +1100

    Added fail safe functionality!

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace TestConsoleApp
         {
             String textFile = @"C:\Users\Justin Zhao\Documents\CyberMondayCustomersTest.csv";
             String[] lines = File.ReadAllLines(textFile);
-            lines = lines.Skip(1).ToArray();
+            //lines = lines.Skip(1).ToArray();
             String API_KEY = "9uxSgUOcEtLuCyLWV6vy";
             String API_SECRET = "xmSBziqxBPbIje2H4Z6BYv6GbLqZkC";
             Boolean HMAC = false;
@@ -23,14 +23,33 @@ namespace TestConsoleApp
             SendMessagesRequest body = new SendMessagesRequest();
             body.Messages = new List<Message>();
             var regex = new Regex(Regex.Escape("0"));
+            var count = 1;
             foreach (string x in lines)
             {
                 Message body_messages_0 = new Message();
                 body_messages_0.Content = x.Split(',')[4].TrimStart().Replace("'", "") + " is offering 5% off all orders placed through YQme on Cyber Monday. Place an order using the following link " + x.Split(',')[5].TrimStart().Replace("'", "") + " and " + x.Split(',')[4].TrimStart().Replace("'", "") + " will give you 5% off your order. \nRegards,\n" + x.Split(',')[4].TrimStart().Replace("'", "");
                 body_messages_0.DestinationNumber = regex.Replace(x.Split(',')[3].TrimStart().Replace("'", ""), "+61", 1);
                 body.Messages.Add(body_messages_0);
+                try
+                {
+                    SendMessagesResponse result = messages.SendMessagesAsync(body).Result;
+                    Console.WriteLine(result);
+                    var tempFile = Path.GetTempFileName();
+                    var linesToKeep = File.ReadLines(textFile).Where(l => l != x);
+                    File.WriteAllLines(tempFile, linesToKeep);
+                    File.Delete(textFile);
+                    File.Move(tempFile, textFile);
+                    Console.WriteLine("SMS " + count + " sent successfully!");
+                }
+                catch (APIException e)
+                {
+                    Console.WriteLine(e.Message + e.ResponseCode + e.HttpContext.ToString());
+                    Console.WriteLine("SMS " + count + " failed!");
+                }
+                body.Messages.Remove(body_messages_0);
+                count++;
             }
-            try
+            /*try
             {
                 SendMessagesResponse result = messages.SendMessagesAsync(body).Result;
                 Console.WriteLine(result);
@@ -38,7 +57,7 @@ namespace TestConsoleApp
             catch (APIException e)
             {
                 Console.WriteLine(e.Message + e.ResponseCode + e.HttpContext.ToString());
-            }
+            }*/
         }
     }
 }
