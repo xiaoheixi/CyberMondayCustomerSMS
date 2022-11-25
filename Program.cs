cce129e75927de2963d27771f6109812fa40e027
xiaoheixi <justinzhao893@gmail.com>
2022-11-25 12:25:05 +1100

    Fixed DestinationNumber starting with +61 and 0.

--- Program.cs
+++ Program.cs
@@ -27,7 +27,14 @@ namespace TestConsoleApp
             {
                 Message body_messages_0 = new Message();
                 body_messages_0.Content = x.Split(',')[4].TrimStart().Replace("'", "") + " is offering 5% off all orders placed through YQme on Cyber Monday. If you place an order using the following link " + x.Split(',')[5].TrimStart().Replace("'", "") + " and " + x.Split(',')[4].TrimStart().Replace("'", "") + " will give you 5% off your order. \nRegards,\n" + x.Split(',')[4].TrimStart().Replace("'", "");
-                body_messages_0.DestinationNumber = regex.Replace(x.Split(',')[3].TrimStart().Replace("'", ""), "+61", 1);
+                if (x.Split(',')[3].TrimStart().Replace("'", "").Contains("+61"))
+                {
+                    body_messages_0.DestinationNumber = x.Split(',')[3].TrimStart().Replace("'", "");
+                }
+                else
+                {
+                    body_messages_0.DestinationNumber = regex.Replace(x.Split(',')[3].TrimStart().Replace("'", ""), "+61", 1);
+                }
                 body.Messages.Add(body_messages_0);
                 try
                 {
