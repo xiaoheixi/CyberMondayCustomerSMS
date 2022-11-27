6190944c086747779575ea32c1796a017fc8230e
xiaoheixi <justinzhao893@gmail.com>
2022-11-28 10:22:50 +1100

    Fixed fatal error for incorrect phone numbers.

--- Program.cs
+++ Program.cs
@@ -27,6 +27,15 @@ namespace TestConsoleApp
             {
                 Message body_messages_0 = new Message();
                 body_messages_0.Content = "Hi " + x.Split(',')[1].TrimStart().Replace("'", "") + ",\n\n" + x.Split(',')[4].TrimStart().Replace("'", "") + " is offering 5% off all orders placed through YQme on Cyber Monday. \n\nOrder now: " + x.Split(',')[5].TrimStart().Replace("'", "") + "\n\nEnjoy!\n" + x.Split(',')[4].TrimStart().Replace("'", "");
+                /*if (x.Split(',')[3].TrimStart().Replace("'", "").Length != 10)
+                {
+                    var tempFile = Path.GetTempFileName();
+                    var linesToKeep = File.ReadLines(textFile).Where(l => l != x);
+                    File.WriteAllLines(tempFile, linesToKeep);
+                    File.Delete(textFile);
+                    File.Move(tempFile, textFile);
+                    continue;
+                }*/
                 if (x.Split(',')[3].TrimStart().Replace("'", "").Contains("+61"))
                 {
                     body_messages_0.DestinationNumber = x.Split(',')[3].TrimStart().Replace("'", "");
@@ -38,6 +47,15 @@ namespace TestConsoleApp
                 else
                 {
                     body_messages_0.DestinationNumber = regex.Replace(x.Split(',')[3].TrimStart().Replace("'", ""), "+61", 1);
+                }
+                if (Regex.IsMatch(body_messages_0.DestinationNumber, "^\\+614[0-9]{8}$") == false)
+                {
+                    var tempFile = Path.GetTempFileName();
+                    var linesToKeep = File.ReadLines(textFile).Where(l => l != x);
+                    File.WriteAllLines(tempFile, linesToKeep);
+                    File.Delete(textFile);
+                    File.Move(tempFile, textFile);
+                    continue;
                 }
                 body.Messages.Add(body_messages_0);
                 try
